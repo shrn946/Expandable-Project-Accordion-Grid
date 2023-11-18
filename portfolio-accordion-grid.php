072ad304afbb31a743edebc073d2adaeb5a7950d
shrn946 <114462856+shrn946@users.noreply.github.com>
2023-11-18 22:25:50 +0500

    Update portfolio-accordion-grid.php

--- portfolio-accordion-grid.php
+++ portfolio-accordion-grid.php
@@ -42,6 +42,8 @@ function expandable_project_accordion_grid_settings_page() {
         <pre>[projects]</pre>
         
         <h3>Video Link</h3>
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/L_UFOW0kxcI?si=ynD3DXibhivZ1WcZ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         
       
     </div>
